b7bc1e67aa8e3fa4250f6bf879a8fcbaddaf5e24
Valerii Stolbov <stolbov.v@outlook.com>
2023-08-07 18:07:07 +0500

    leason #1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,25 @@ window.addEventListener('DOMContentLoaded', () => {
   const movieDB = {
     movies: ['Логан', 'Лига справедливости', 'Ла-ла лэнд', 'Одержимость', 'Скотт Пилигрим против...'],
   };
+
+  const adv = document.querySelectorAll('.promo__adv img');
+  const poster = document.querySelector('.promo__bg');
+  const genre = poster.querySelector('.promo__genre');
+  const movieList = document.querySelector('.promo__interactive-list');
+
+  adv.forEach((item) => {
+    item.remove();
+  });
+
+  genre.textContent = 'драма';
+
+  poster.style.backgroundImage = 'url("../images/bg.jpg")';
+
+  movieList.innerHTML = '';
+
+  movieDB.movies.sort();
+
+  movieDB.movies.forEach((filme, index) => {
+    movieList.innerHTML += `<li class="promo__interactive-item">${index + 1}. ${filme}<div class="delete"></div></li>`;
+  });
 });
